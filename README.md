32fee21530eb94c860999a722a1d0996625e6226
Abhishek Parab <abhishekparab@gmail.com>
2020-11-02 22:56:40 -0600

    Initial commit after downloading data.

---
+++ README.md
@@ -0,0 +1,2 @@
+# png2pgn
+Identify chess positions from png/jpeg files containing 2D positions. 
---
+++ code/01_download_kaggle_dataset.py
@@ -0,0 +1,12 @@
+#############################################
+# Download chessboards' dataset from Kaggle
+#############################################
+
+# My authentication is using google. 
+
+# > conda install kaggle
+
+# Need to generate a `kaggle.json` file from Kaggle website after logging in and paste it at `/home/msabp/.kaggle/kaggle.json` then `chmod 600 kaggle.json` etc. Once that is done, download the dataset using
+
+# > kaggle datasets download -d koryakinp/chess-positions
+# > unzip chess_datasets.zip
